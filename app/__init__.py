3403e16f4f39952de58d2c7045bfb780019018de
Jaystaks <jayson.waigwa@riarauniversity.ac.ke>
2019-02-18 12:20:29 +0300

    [minor fix]

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,21 @@
 from flask import Flask, Blueprint
 from app.api.v1.views.politicalparty import politicalparty
 from app.api.v1.views.politicaloffices import politicaloffice
+from app.api.db.dbconnect import init_app
 from instance.config import appConfig
-from werkzeug.exceptions import HTTPException
-
-
-def method_not_json(e):
-	"""
-    Capture Not Found error.
-    """
-
-	return make_response(jsonify({
-		"status" : "bad request",
-		"message" : "method not allowed, invalid json format"
-        }), 400)
-
-def page_not_found(e):
-	"""
-    Capture Not Found error.
-    """
-
-	return make_response(jsonify({
-		"status" : "not found",
-		"message" : "url does not exist"
-        }), 404)
 
 def politico(config_name):
     app= Flask(__name__)
 
     app.config.from_object(appConfig[config_name])
+    app.config.from_pyfile('config.py')
+    app.config["JSON_SORT_KEYS"] = False
+
 
     app.register_blueprint(politicalparty, url_prefix="/api/v1")
     app.register_blueprint(politicaloffice, url_prefix="/api/v1/")
 
-    app.register_blueprint(politicaloffice, url_prefix="/api/v1")
+
+    init_app(app)
 
     return app
