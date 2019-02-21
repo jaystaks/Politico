23d00da2d803b339c3f34b70b3ed646aa49d146c
0v3rfl0w <jay5taks@localhost.localdomain>
2019-02-21 23:20:36 +0300

    [finishes #163969793] Finished Configuring a database

---
+++ app/api/db/dbschema.py
@@ -0,0 +1,62 @@
+schema = """
+
+DROP TABLE IF EXISTS offices CASCADE;
+
+CREATE TABLE offices(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS parties CASCADE;
+
+CREATE TABLE parties(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    hqaddress VARCHAR(100) NOT NULL,
+    logoUrl VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users(
+   id SERIAL NOT NULL PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    othername VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    phonenumber VARCHAR(100) NOT NULL,
+    passporturl VARCHAR(100) NOT NULL,
+    isadmin BOOLEAN DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS candidates CASCADE;
+
+CREATE TABLE candidates(
+    id SERIAL NOT NULL PRIMARY KEY,
+    office INT REFERENCES offices(id),
+    party INT  REFERENCES parties(id),
+    candidate INT UNIQUE REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS votes CASCADE;
+
+CREATE TABLE votes(
+    id SERIAL NOT NULL PRIMARY KEY,
+    createdon TIMESTAMP DEFAULT now(),
+    createdby INT REFERENCES users(id),
+    office INT REFERENCES offices(id),
+    candidate INT UNIQUE REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS petition CASCADE;
+
+CREATE TABLE petition(
+    id SERIAL NOT NULL PRIMARY KEY,
+    createdon TIMESTAMP DEFAULT now(),
+    createdby INT REFERENCES users(id),
+    office INT REFERENCES offices(id),
+    body TEXT NOT NULL
+);
+"""
