d5172747b46b6f71cd3e72c2fce319f4ce541dee
Jaystaks <jayson.waigwa@riarauniversity.ac.ke>
2019-03-02 00:04:20 +0300

    [finishes #163970519]Finished on user can view results

---
+++ app/api/v2/views/resultsviews.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, jsonify, make_response
+from flask_jwt_extended import jwt_required
+
+from app.api.v2.models.resultsmodel import ResultsModel
+
+result = Blueprint('results', __name__, url_prefix="/api/v2")
+
+
+@result.route("/office/<id>/result", methods=['GET'])
+@jwt_required
+def api_results(id):
+    if not isinstance(id, int):
+        return make_response(jsonify({"status": 400, "error": "Invalid Office Id"}), 400)
+    results = ResultsModel(id).get_results()
+    if not isinstance(results, list):
+        return make_response(jsonify({"status": 404, "error": "Results Not Found"}), 404)
+    return make_response(jsonify({"status": 200,
+                                  "data": [{
+                                      "office": id,
+                                      "candidate": results[0][0],
+                                      "results": results[0][1]
+                                  }]}), 200)
